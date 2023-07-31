679aea7bbe292422cddc94c4aab52bf2beaa84ca
Arnab048 <codingjedi048@gmail.com>
2023-07-31 22:36:54 +0600

    finalization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const Info = [
+    {name:'Arnab',id:'048'},
+    {name:'Redoy',id:'050'},
+    {name:'Shovon',id:'014'}
+  ];
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I'm learning ReactJS
-        </p>
-        <a
-          className="App-link"
-          href="https://algobot048.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello</p>
+        <Counter></Counter>
+        {
+          Info.map((it)=>
+            <Person props={it}></Person >
+          )
+        }
       </header>
     </div>
   );
+}
+
+function Counter(){
+  const [count,setCount]=useState(69);
+  const IncreaseCount=()=>{
+    const newCount=count+1;
+    setCount(newCount);
+  }
+  const DecreaseCount=()=>{
+    const newCount=count-1;
+    setCount(newCount);
+  }
+  return (
+    <div>
+      <h1>
+        Count: {count}
+      </h1>
+      <button onClick={IncreaseCount}>
+        Increase
+      </button>
+      <button onClick={DecreaseCount}>
+        Decrease
+      </button>
+    </div>
+  )
+}
+
+function Person({props}) {
+  const personStyle = {
+    border: '2px solid yellow',
+    margin: '10px'
+  };
+  return (
+    <div style={personStyle}>
+      <h1>
+        {props.name}
+      </h1>
+      <h3>
+        {props.id}
+      </h3>
+    </div>
+  )
 }
 
 export default App;
